5ba3eafc0a4f51db083383e6c1fce09ff03a1495
Kazimierz Filip <Kazimierz.L.Filip@gmail.com>
2018-05-30 10:35:40 +0200

    Menu select routine added, variables name changed

--- app/src/main/java/com/wordpress/javawrok/grind/EditListActivity.java
+++ app/src/main/java/com/wordpress/javawrok/grind/EditListActivity.java
@@ -1,20 +1,14 @@
 package com.wordpress.javawrok.grind;
 
-import android.app.Activity;
 import android.content.Context;
-import android.content.Intent;
 import android.content.SharedPreferences;
-import android.net.Uri;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-import java.util.Collection;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.Set;
 
 public class EditListActivity extends AppCompatActivity {
@@ -31,7 +25,7 @@ public class EditListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_edit_list);
 
 
-        insertedTasks = findViewById(R.id.insert_tasks_et);
+        insertedTasks = findViewById(R.id.edit_insert_tasks);
         saveButton = findViewById(R.id.save_button);
 
         sharedPref = getPreferences(Context.MODE_PRIVATE);
--- app/src/main/java/com/wordpress/javawrok/grind/MainActivity.java
+++ app/src/main/java/com/wordpress/javawrok/grind/MainActivity.java
@@ -1,10 +1,14 @@
 package com.wordpress.javawrok.grind;
 
 import android.content.Context;
-import android.content.Intent;
-import android.content.SharedPreferences;
+import android.os.Build;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -12,62 +16,118 @@ import android.widget.TextView;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
-import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String PREFERENCES_TAG = "to_do_list";
-    private static final int REQUEST_TASKS = 100;
+    private static int sCount;
+    private static int sRoutine;
+    private static final String POSITION = "pos";
+    private static final String ROUTINE = "rut";
+    private Vibrator mVibrator;
 
-//    private Set<String> list;
-    private ArrayList<String> list;
-    private TextView viewTask;
-    private Button nextButton;
-    private Iterator<String> listIterator;
-    private SharedPreferences sharedPref;
+    private ArrayList<String> mStringArrayList;
+    private TextView mTextViewTask;
+    private Button mButtonNext;
+    private Iterator<String> mStringIterator;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.activity_main,menu);
+        return true;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("");
 
-        sharedPref = this.getPreferences(Context.MODE_PRIVATE);
-        list = new ArrayList<>(Arrays.asList(getResources().getStringArray(R.array.list)));
+        mStringArrayList = new ArrayList<>();
+        mStringArrayList.add("Welcome");
 
-//        list = sharedPref.getStringSet(PREFERENCES_TAG, null);
-//        if(list==null){
-//            Intent getListIntent = new Intent(this, EditListActivity.class);
-//            startActivityForResult(getListIntent,REQUEST_TASKS);
-//        } else {
-            listIterator = list.iterator();
+        if(savedInstanceState!=null) {
+            int routine = savedInstanceState.getInt(ROUTINE);
+            if(routine == 1){
+                mStringArrayList = new ArrayList<>(
+                        Arrays.asList(getResources().getStringArray(R.array.list1)));
+                sRoutine=1;
+            } else if(routine == 2) {
+                mStringArrayList = new ArrayList<>(
+                        Arrays.asList(getResources().getStringArray(R.array.list2)));
+                sRoutine = 2;
+            }
+        }
 
-            viewTask = findViewById(R.id.task_tv);
-        if(listIterator.hasNext())
-            viewTask.setText(listIterator.next());
-//        }
-        nextButton = findViewById(R.id.next_button);
-        nextButton.setOnClickListener(new View.OnClickListener() {
+        mVibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+
+        mStringIterator = mStringArrayList.iterator();
+
+        mTextViewTask = findViewById(R.id.text_task);
+
+        if(savedInstanceState!=null){
+            int pos = savedInstanceState.getInt(POSITION);
+            for(int i=0; i<pos; i++){
+                mStringIterator.next();
+            }
+        }
+        displayItem();
+        mButtonNext = findViewById(R.id.button_next);
+        mButtonNext.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(listIterator.hasNext())
-                    viewTask.setText(listIterator.next()+".");
-                else
-                    viewTask.setText("done.");
+                displayItem();
             }
         });
     }
 
-//    @Override
-//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-//        if (requestCode == REQUEST_TASKS) {
-//            if (resultCode == RESULT_OK) {
-//                list = sharedPref.getStringSet(PREFERENCES_TAG, null);
-//                listIterator = list.iterator();
-//
-//                viewTask = findViewById(R.id.task_tv);
-//
-//                viewTask.setText(listIterator.next());
-//            }
-//        }
-//    }
+    void vibration(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            mVibrator.vibrate(VibrationEffect.createOneShot(500,VibrationEffect.DEFAULT_AMPLITUDE));
+        }else{
+            //deprecated in API 26
+            mVibrator.vibrate(500);
+        }
+    }
+
+    void displayItem(){
+        if(mStringIterator.hasNext()){
+            vibration();
+            String s = mStringIterator.next();
+            if(!s.endsWith(".") && !s.endsWith("?") && !s.endsWith("!"))
+                s=s+".";
+            mTextViewTask.setText(s);
+            sCount += 1;
+        }
+        else{
+            sCount = 0;
+            mTextViewTask.setText("done.");
+        }
+    }
+
+    public void showRoutine1(MenuItem item){
+        mStringArrayList = new ArrayList<>(
+                Arrays.asList(getResources().getStringArray(R.array.list1)));
+        sRoutine = 1;
+        sCount = 0;
+        mStringIterator = mStringArrayList.iterator();
+        displayItem();
+    }
+
+    public void showRoutine2(MenuItem item){
+        mStringArrayList = new ArrayList<>(
+                Arrays.asList(getResources().getStringArray(R.array.list2)));
+        sRoutine=2;
+        sCount = 0;
+        mStringIterator = mStringArrayList.iterator();
+        displayItem();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(POSITION, sCount);
+        outState.putInt(ROUTINE, sRoutine);
+    }
+
 }
